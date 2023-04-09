d9d0c7b041c7f9d5e80f72402bb7d685ab9e34b8
Radek <plocharadoslaw@gmail.com>
2023-04-09 21:24:49 +0200

    added loading component and playing music animation

---
+++ src/components/Animate/SongAnimation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './wave.css';
+
+export default function SongAnimation() {
+  return (
+    <div className="boxContainer">
+      <div className="box box1"></div>
+      <div className="box box2"></div>
+      <div className="box box3"></div>
+      <div className="box box4"></div>
+    </div>
+  );
+}
