bd2d62b2f89188f49ec51002c6e896d5e2033d5a
Radek <plocharadoslaw@gmail.com>
2023-04-10 09:45:16 +0200

    added expiresAt var

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const useAuth = (code: string) => {
   const [accessToken, setAccessToken] = React.useState();
-  const [refreshToken, setRefleshToken] = React.useState();
+  const [refreshToken, setRefreshToken] = React.useState();
   const [expiresIn, setExpiresIn] = React.useState();
 
   
@@ -11,15 +11,17 @@ const useAuth = (code: string) => {
   useEffect(() => {
     if (!code) return;
     axios
-      .post('http://localhost:5000/auth/login', {
+      .post('https://music-backend-2hi1.onrender.com/auth/login', {
         code,
       })
       .then((res) => {
+          console.log(Date.now() + res.data.expiresIn * 1000);
+
         setAccessToken(res.data.accessToken);
         localStorage.setItem('accessToken', res.data.accessToken);
-        console.log(res.data.accessToken);
-        
-        setRefleshToken(res.data.refreshToken);
+        localStorage.setItem("expiresAt", `${Date.now() + res.data.expiresIn * 1000}`);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+        setRefreshToken(res.data.refreshToken);
         setExpiresIn(res.data.expiresIn);
         window.history.pushState({}, '', '/');
       })
@@ -33,13 +35,15 @@ const useAuth = (code: string) => {
     if (!refreshToken || !expiresIn) return;
     const interval = setInterval(() => {
       axios
-        .post('http://localhost:5000/auth/refresh', {
+        .post('https://music-backend-2hi1.onrender.com/auth/refresh', {
           refreshToken,
         })
         .then((res) => {
+          
           setAccessToken(res.data.accessToken);
           localStorage.setItem('accessToken', res.data.accessToken);
-
+          localStorage.setItem("expiresAt", `${Date.now() + res.data.expiresIn * 1000}`);
+          
           setExpiresIn(res.data.expiresIn);
           // window.history.pushState({}, null, '/');
         })
