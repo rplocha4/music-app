452cea2ea3150592934d5b6bd2f8516a6e92bda3
Radek <plocharadoslaw@gmail.com>
2023-04-16 11:32:44 +0200

    renamed page

---
+++ src/pages/SpotifyAuth.tsx
@@ -0,0 +1,34 @@
+const redirectUri = 'http://localhost:5173';
+
+const client_id = '36308efa2dd24d35a35792a4c4698fda';
+
+const scopses = [
+  'user-top-read',
+  'playlist-modify-private',
+  'playlist-modify-public',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-top-read',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+];
+
+export const loginUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirectUri}&scope=${scopses.join(
+  '%20'
+)}&response_type=code&show_dialog=true`;
+function SpotifyAuth() {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <a className="bg-green-400 rounded-xl p-5 text-2xl " href={loginUrl}>
+        Login with Spotify
+      </a>
+    </div>
+  );
+}
+
+export default SpotifyAuth;
