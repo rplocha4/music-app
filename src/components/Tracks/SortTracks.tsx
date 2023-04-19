c82fc299976228bea72d069b77e8c792644769ca
Radek <plocharadoslaw@gmail.com>
2023-04-19 10:47:51 +0200

    adde sorting by clicking name

--- src/components/Tracks/SortTracks.tsx
+++ src/components/Tracks/SortTracks.tsx
@@ -4,10 +4,11 @@ import {
   MdOutlineKeyboardArrowDown,
   MdOutlineKeyboardArrowUp,
 } from 'react-icons/md';
+import { BiTime } from 'react-icons/bi';
 const options = [
   {
-    name: 'Name',
-    label: 'Name',
+    name: 'Title',
+    label: 'Title',
   },
   {
     name: 'Date',
@@ -21,6 +22,10 @@ const options = [
     name: 'Duration',
     label: 'Duration',
   },
+  {
+    name: 'Artist',
+    label: 'Artist',
+  },
 ];
 
 const SortTracks: React.FC<{
@@ -28,14 +33,14 @@ const SortTracks: React.FC<{
   onSort: (tracks: { track: TrackItem; added: string }[]) => void;
 }> = ({ tracks, onSort }) => {
   const [sortAscending, setSortAscending] = React.useState(false);
-  const [sortType, setSortType] = React.useState('Name');
+  const [sortType, setSortType] = React.useState('Title');
   const [hover, setHover] = React.useState(false);
 
   //sort by name, artist, album, date added
   useEffect(() => {
     const sorted = [
       ...tracks.sort((a, b) => {
-        if (sortType === 'Name') {
+        if (sortType === 'Title') {
           return sortAscending
             ? a.track.name
                 .toLowerCase()
@@ -63,55 +68,174 @@ const SortTracks: React.FC<{
             ? a.track.duration_ms - b.track.duration_ms
             : b.track.duration_ms - a.track.duration_ms;
         }
+        if (sortType === 'Artist') {
+          return sortAscending
+            ? a.track.artists[0].name
+                .toLowerCase()
+                .localeCompare(b.track.artists[0].name.toLowerCase())
+            : b.track.artists[0].name
+                .toLowerCase()
+                .localeCompare(a.track.artists[0].name.toLowerCase());
+        }
+        return 0;
       }),
     ];
     onSort(sorted);
   }, [sortType, sortAscending]);
   return (
-    <div className="self-end mr-10 w-20">
-      <div className="relative ">
-        <span
-          onClick={() => setHover((prev) => !prev)}
-          className="flex items-center justify-center cursor-pointer"
-        >
-          <>
-            {sortType}{' '}
-            {sortAscending ? (
-              <MdOutlineKeyboardArrowUp className="font-bold text-2xl" />
-            ) : (
-              <MdOutlineKeyboardArrowDown className="font-bold text-2xl" />
-            )}
-          </>
-        </span>
-        {hover && (
-          <div className="absolute bg-zinc-700 flex flex-col -left-full items-start text-sta justify-start w-40 rounded-lg z-20">
-            <p className="text-gray-400 text-sm font-bold p-2">Sort by</p>
-            {options.map((option) => (
-              <button
-                key={option.name}
-                onMouseDown={() => {
-                  sortType === option.name
-                    ? setSortAscending(!sortAscending)
-                    : setSortAscending(true);
-                  setSortType(option.name);
-                }}
-                className={`rounded-lg hover:bg-zinc-600 flex items-center justify-start${
-                  sortType === option.name && ' text-green-600'
-                } w-full h-full p-2`}
-              >
-                Sort by {option.label}{' '}
-                {sortType === option.name &&
-                  (sortAscending ? (
-                    <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
-                  ) : (
-                    <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
-                  ))}
-              </button>
-            ))}
-          </div>
-        )}
+    <>
+      <div className="self-end mr-10 w-20">
+        <div className="relative ">
+          <span
+            onClick={() => setHover((prev) => !prev)}
+            className="flex items-center justify-center cursor-pointer"
+          >
+            <>
+              {sortType}{' '}
+              {sortAscending ? (
+                <MdOutlineKeyboardArrowUp className="font-bold text-2xl" />
+              ) : (
+                <MdOutlineKeyboardArrowDown className="font-bold text-2xl" />
+              )}
+            </>
+          </span>
+          {hover && (
+            <div className="absolute bg-zinc-700 flex flex-col -left-full items-start text-sta justify-start w-40 rounded-lg z-20">
+              <p className="text-gray-400 text-sm font-bold p-2">Sort by</p>
+              {options.map((option) => (
+                <button
+                  key={option.name}
+                  onMouseDown={() => {
+                    sortType === option.name
+                      ? setSortAscending(!sortAscending)
+                      : setSortAscending(true);
+                    setSortType(option.name);
+                  }}
+                  className={`rounded-lg hover:bg-zinc-600 flex items-center justify-start${
+                    sortType === option.name && ' text-green-600'
+                  } w-full h-full p-2`}
+                >
+                  Sort by {option.label}{' '}
+                  {sortType === option.name &&
+                    (sortAscending ? (
+                      <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+                    ) : (
+                      <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+                    ))}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className="grid grid-cols-6 gap-10 py-2 w-full text-left font-bold border-b border-gray-400">
+        <div className="col-span-2 flex gap-12 items-center  text-center">
+          <span className="w-10">#</span>
+          <div className="flex items-center gap-5 justify-center">
+            <span
+              className="flex justify-center items-center cursor-default"
+              onMouseDown={() => {
+                sortType === 'Title'
+                  ? setSortAscending(!sortAscending)
+                  : setSortAscending(true);
+                setSortType('Title');
+              }}
+            >
+              Title
+              {sortType === 'Title' ? (
+                sortAscending ? (
+                  <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+                ) : (
+                  <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+                )
+              ) : (
+                <p className="w-6"></p>
+              )}
+            </span>
+            <span
+              onMouseDown={() => {
+                sortType === 'Artist'
+                  ? setSortAscending(!sortAscending)
+                  : setSortAscending(true);
+                setSortType('Artist');
+              }}
+              className="flex justify-center items-center cursor-default"
+            >
+              Artist
+              {sortType === 'Artist' ? (
+                sortAscending ? (
+                  <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+                ) : (
+                  <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+                )
+              ) : (
+                <p className="w-6"></p>
+              )}
+            </span>
+          </div>
+        </div>
+        <span
+          className="col-span-2 flex justify-center items-center cursor-default"
+          onMouseDown={() => {
+            sortType === 'Album'
+              ? setSortAscending(!sortAscending)
+              : setSortAscending(true);
+            setSortType('Album');
+          }}
+        >
+          Album
+          {sortType === 'Album' ? (
+            sortAscending ? (
+              <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+            ) : (
+              <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+            )
+          ) : (
+            <p className="w-6"></p>
+          )}
+        </span>
+        <span
+          className="flex justify-center items-center cursor-default"
+          onMouseDown={() => {
+            sortType === 'Date'
+              ? setSortAscending(!sortAscending)
+              : setSortAscending(true);
+            setSortType('Date');
+          }}
+        >
+          Date Added{' '}
+          {sortType === 'Date' ? (
+            sortAscending ? (
+              <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+            ) : (
+              <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+            )
+          ) : (
+            <p className="w-6"></p>
+          )}
+        </span>
+        <span
+          className="flex items-center justify-center"
+          onMouseDown={() => {
+            sortType === 'Duration'
+              ? setSortAscending(!sortAscending)
+              : setSortAscending(true);
+            setSortType('Duration');
+          }}
+        >
+          <BiTime />
+          {sortType === 'Duration' ? (
+            sortAscending ? (
+              <MdOutlineKeyboardArrowUp className="font-bold text-2xl text-green-600" />
+            ) : (
+              <MdOutlineKeyboardArrowDown className="font-bold text-2xl text-green-600" />
+            )
+          ) : (
+            <p className="w-6"></p>
+          )}
+        </span>
+      </div>
+    </>
   );
 };
 
--- src/components/Tracks/TrackResultsSortable.tsx
+++ src/components/Tracks/TrackResultsSortable.tsx
@@ -3,6 +3,7 @@ import { TrackItem } from '../../types/types';
 import TrackCard from './TrackCard';
 import TrackCardSortable from './TrackCardSortable';
 import SortTracks from './SortTracks';
+import { BiTime } from 'react-icons/bi';
 
 const TrackResultsSortable: React.FC<{
   tracks: { track: TrackItem; added: string }[];
@@ -14,21 +15,8 @@ const TrackResultsSortable: React.FC<{
   };
 
   return (
-    <div className="flex flex-col text-white">
+    <div className="flex flex-col text-white py-5">
       <SortTracks tracks={tracks} onSort={handleSort} />
-      {/* <button
-        onClick={() => {
-          sortType === 'name'
-            ? setSortAscending(!sortAscending)
-            : setSortAscending(true);
-          setSortType('name');
-        }}
-      >
-        Sort by name
-      </button>
-      <button onClick={() => setSortType('date')}>Sort by date</button>
-      <button onClick={() => setSortType('album')}>Sort by album</button>
-      <button onClick={() => setSortType('duration')}>Sort by duration</button> */}
 
       {sortedTracks.map((track, i) => (
         <TrackCardSortable
