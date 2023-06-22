2ca50692db7c62df372454405290e67460a312cf
Radek <plocharadoslaw@gmail.com>
2023-06-22 15:39:22 +0200

    updated error handing

--- src/pages/errorPage.tsx
+++ src/pages/errorPage.tsx
@@ -6,9 +6,24 @@ import { showInfo } from '../store/uiSlice';
 export default function ErrorPage() {
   const error: any = useRouteError();
   // console.error(error);
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
   dispatch(showInfo('Something went wrong! Please try again later!'));
 
-  return <Navigate to="/" replace />;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-zinc-700 text-white">
+      <h1 className="text-3xl font-bold">
+        Something went wrong! Please try again later!
+      </h1>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+        className="mt-5 rounded-lg bg-blue-500 px-5 py-2 text-white"
+      >
+        Go back to home page
+      </button>
+    </div>
+  );
 }
