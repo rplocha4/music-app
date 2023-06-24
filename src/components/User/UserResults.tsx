40707bd7e4ac6f712dfd0bf9a08cb0f5450c4c30
Radek <plocharadoslaw@gmail.com>
2023-06-24 12:00:54 +0200

    removed console.log

--- src/components/User/UserResults.tsx
+++ src/components/User/UserResults.tsx
@@ -3,7 +3,6 @@ import { FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const UserResults: React.FC<{ users: any }> = ({ users }) => {
-  console.log(users);
 
   return (
     <>
